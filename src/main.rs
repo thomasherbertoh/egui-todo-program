0b5084f4384c65be9bbe82f16fbdced1c56052e0
thomashertbertoh <thomasjackherbert@gmail.com>
2022-05-07 09:13:10 +0200

    upgraded to latest versions of egui, epi, and eframe

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ mod todo_list;
 fn main() {
     let app = todo_list::TodoList::default();
     let native_options = eframe::NativeOptions::default();
-    eframe::run_native(Box::new(app), native_options);
+    eframe::run_native("Todo List", native_options, Box::new(|_| Box::new(app)));
 }
